f7ef7404d38687487668fef3123d87a0d1ac2d96
Ruban <39084243+reuban-zacker@users.noreply.github.com>
2019-09-09 13:37:39 +0530

    Add files via upload

---
+++ AI programming with python/Dogbreed Classifier/dogbreed_classifier/run_models_batch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
+#                                                                             
+# PROGRAMMER: Ruban B
+# DATE CREATED: 27 mar 2019                                  
+# REVISED DATE: 
+# PURPOSE: Runs all three models to test which provides 'best' solution.
+#          Please note output from each run has been piped into a text file.
+#
+# Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
+#  
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
+
