a67d65dd01911ead954c142504437307c0f4f41b
Ruban <39084243+reuban-zacker@users.noreply.github.com>
2020-03-07 22:42:53 +0530

    Add files via upload

---
+++ C++/3) Memory Management Chatbot/src/graphnode.cpp
@@ -0,0 +1,63 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    // delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot.reset(new ChatBot()); // Needed to reset otherwise it segfaults at move assignment operator
+    *_chatBot = std::move(chatbot); // This should invoke the move constructor
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(*_chatBot)); // Added std::move to convert lvalue to rvalue
+                                                    // otherwise it was invoking copy constructor when sending a query.
+    _chatBot.reset();
+    // _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
